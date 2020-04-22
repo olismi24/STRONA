97a70e8e929b4a254eec9a8245c01915d9285d60
olismi24 <64143202+olismi24@users.noreply.github.com>
2020-04-22 15:57:10 +0200

    Strona-kolory

---
+++ kolory_OS.js
@@ -0,0 +1,41 @@
+const redBtn = document.querySelector('.red');
+ 
+    redBtn.addEventListener('click', function(){
+        document.body.className = "";
+        document.body.className = "red";
+    })
+	
+const orangeBtn = document.querySelector('.orange');
+ 
+    orangeBtn.addEventListener('click', function(){
+        document.body.className = "";
+        document.body.className = "orange";
+    })
+	
+const yellowBtn = document.querySelector('.yellow');
+ 
+    yellowBtn.addEventListener('click', function(){
+        document.body.className = "";
+        document.body.className = "yellow";
+    })
+	
+const greenBtn = document.querySelector('.green');
+ 
+    greenBtn.addEventListener('click', function(){
+        document.body.className = "";
+        document.body.className = "green";
+    })
+	
+const blueBtn = document.querySelector('.blue');
+ 
+    blueBtn.addEventListener('click', function(){
+        document.body.className = "";
+        document.body.className = "blue";
+    })
+	
+const purpleBtn = document.querySelector('.purple');
+ 
+    purpleBtn.addEventListener('click', function(){
+        document.body.className = "";
+        document.body.className = "purple";
+    })
